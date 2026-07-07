be425f9de035a984fc738523d6f1c558a3aeb265
Juan Sebastián González López <juangonzalezlopez241829@gmail.com>
2026-07-07 12:48:44 -0600

    test

--- src/actions/n8n.ts
+++ src/actions/n8n.ts
@@ -7,7 +7,7 @@ interface NotifyDiscordInput {
 export const notifyDiscord = createServerFn({method: 'POST'})
     .inputValidator((data: NotifyDiscordInput) => data)
     .handler(async ({data}) => {
-        await fetch("https://n8n.squareprojects.dev/webhook-test/d30f7c3f-af8e-43ec-9dcf-8b5c5466ef1c", {
+        await fetch("https://n8n.squareprojects.dev/webhook/d30f7c3f-af8e-43ec-9dcf-8b5c5466ef1c", {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
